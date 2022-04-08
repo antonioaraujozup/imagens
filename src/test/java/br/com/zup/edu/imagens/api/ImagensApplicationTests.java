b5d04d10f3e73cc41ddbe5548975c601dfb7d36f
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-08 17:46:01 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/edu/imagens/api/ImagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.imagens.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
